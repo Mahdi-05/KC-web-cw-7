a56cf619229563c752e087dba808292c0dc30b4c
مهدي جعفر العلي <109863442+Mahdi-05@users.noreply.github.com>
2022-08-17 23:37:22 +0300

    Done

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+function BMI (weight=50,height=1.60) {
+    let result= weight /(height * height)
+    return result
+    
+}
 
+console.log(BMI(90,1.80))
+console.log(BMI())
+
+function Status(bmi){
+    let r = document.getElementById("result")
+
+    if(bmi<18.5){
+    r.style.color="orange"
+        return `لديك نقص في الوزن`
+        
+    }else if(bmi >= 18.5 && bmi<25 ){
+       r.style.color="green"
+        return`وزنك صحي`
+
+        
+    }else if(bmi>=25){
+        r.style.color="red"
+        return `لديك زيادة في الوزن`
+
+    }
+}
+ function calculate () {
+    let weight = document.getElementById(`weight`).value
+    let height = document.getElementById(`height`).value
+
+
+    let bmi = BMI(weight,height)
+    let desc = Status(bmi)
+
+    let div = document.getElementById('result')
+    div.innerText = bmi + " == " + desc
+
+ }
